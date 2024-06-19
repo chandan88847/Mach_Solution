d241bb42809acf1092fb250bdcb7e16a796bbdf9
Kiran <kiran.mh@valtech.com>
2024-06-19 12:09:01 +0530

    UserAPI added

---
+++ Microservices/UserAPI/Data/UserDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using UserAPI.Model;
+
+namespace UserAPI.Data
+{
+    public class UserDbContext : DbContext
+    {
+        public UserDbContext(DbContextOptions<UserDbContext> options) : base(options)
+        {
+        }
+        public DbSet<User> UserProfiles { get; set; }
+    }
+}
---
+++ Microservices/UserAPI/Model/User.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using AuthenticationAPI.Models;
+
+namespace UserAPI.Model
+{
+    public class User
+    {
+        [Key]
+        public int UserProfileId { get; set; }
+
+        [ForeignKey("ApplicationUser")]
+        public string ApplicationUserId { get; set; }
+        public ApplicationUser ApplicationUser { get; set; }
+
+        //public byte[] UserPhoto { get; set; }
+        public bool EmailVerified { get; set; }
+        public bool PhoneNumberVerified { get; set; }
+        public DateOnly DateOfBirth { get; set; }
+        public string Gender { get; set; }
+        public string City { get; set; }
+        public string Address { get; set; }
+        public string PostalCode { get; set; }
+        public int KYCStatus { get; set; }
+        public string DrivingLicenseId { get; set; }
+        public string Security_Question { get; set; }
+        public string Security_Answer { get; set; }
+    }
+}
