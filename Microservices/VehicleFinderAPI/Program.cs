9cd56083943e8d7b5b34f98ae36f6844e2f13a67
Kiran <kiran.mh@valtech.com>
2024-06-21 11:55:42 +0530

    Vehicle API and getvehiclebylocation

--- Microservices/VehicleFinderAPI/Program.cs
+++ Microservices/VehicleFinderAPI/Program.cs
@@ -1,9 +1,24 @@
+using Microsoft.EntityFrameworkCore;
+using UserAPI.Data;
+using VehicleFinderAPI.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 
+builder.Services.AddDbContext<VehicleDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAllOrigins",
+        builder => builder
+            .AllowAnyOrigin()
+            .AllowAnyHeader()
+            .AllowAnyMethod());
+});
+builder.Services.AddScoped<VehicleService>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
---
+++ Microservices/VehicleFinderAPI/Services/VehicleService.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UserAPI.Data;
+using VehicleFinderAPI.Models;
+
+namespace VehicleFinderAPI.Services
+{
+    public class VehicleService
+    {
+        private readonly VehicleDbContext _context;
+
+        public VehicleService(VehicleDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<VehicleDetails>> GetAllVehiclesAsync()
+        {
+            return await _context.VehicleDetails.ToListAsync();
+        }
+
+        public async Task<VehicleDetails> GetVehicleByIdAsync(string itemId)
+        {
+            return await _context.VehicleDetails.FindAsync(itemId);
+        }
+
+        public async Task<VehicleDetails> CreateVehicleAsync(VehicleDetails vehicleDetails)
+        {
+            _context.VehicleDetails.Add(vehicleDetails);
+            await _context.SaveChangesAsync();
+            return vehicleDetails;
+        }
+
+        public async Task<VehicleDetails> UpdateVehicleAsync(VehicleDetails vehicleDetails)
+        {
+            _context.Entry(vehicleDetails).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+            return vehicleDetails;
+        }
+
+        public async Task<bool> DeleteVehicleAsync(string itemId)
+        {
+            var vehicle = await _context.VehicleDetails.FindAsync(itemId);
+            if (vehicle == null)
+            {
+                return false;
+            }
+
+            _context.VehicleDetails.Remove(vehicle);
+            await _context.SaveChangesAsync();
+            return true;
+        }
+
+        public async Task<IEnumerable<VehicleDetails>> GetVehicleByLocationAsync(string location)
+        {
+            return await _context.VehicleDetails.Where(v => v.Location == location).ToListAsync();
+        }
+    }
+}
